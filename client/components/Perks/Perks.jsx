64039877486abebdbc7bc07009878b82146da531
Nantabo Naome <150276188+Nantabo@users.noreply.github.com>
2023-12-23 17:33:27 +0300

    Add files via upload

---
+++ client/components/Perks/Perks.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+
+function Perks({selected,onChange}){
+
+    function handleCbClick(ev){
+        const {checked,name} = ev.target;
+
+        if(checked){
+            onChange([...selected,name]);
+        }else{
+            onChange([...selected.filter(selectedName => selectedName !== name)])
+        }
+        
+    }
+
+
+    return(
+        <>
+            <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+                <input type="checkbox" checked={selected.includes('wifi')} name="wifi" onChange={handleCbClick}/>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z" />
+</svg>
+                <span>Wifi</span>
+                </label>
+                <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer" >
+                <input type="checkbox" checked={selected.includes('parking')} name="parking" onChange={handleCbClick}/>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
+</svg>
+                <span>Parking</span>
+                </label>
+          
+                <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer" >
+                <input type="checkbox" checked={selected.includes('tv')} name="tv" onChange={handleCbClick}/>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125z" />
+</svg>
+                <span>TV</span>
+                </label>
+                <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+                <input type="checkbox" checked={selected.includes('conditioning')} name="conditioning" onChange={handleCbClick}/>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3" />
+</svg>
+
+                <span>Air conditioning</span>
+                </label>
+
+                <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+                <input type="checkbox" checked={selected.includes('pets')} name="pets" onClick={handleCbClick}/>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z" />
+</svg>
+                <span>Pets</span>
+                </label>
+       
+                <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+                <input type="checkbox" checked={selected.includes('entrance')} name="entrance" onChange={handleCbClick}/>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+</svg>
+
+                <span>Private Entrance</span>
+                </label>
+            
+                <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+                <input type="checkbox" checked={selected.includes('pool')} name="pool" onClick={handleCbClick}/>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 01-1.125-1.125v-3.75zM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-8.25zM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-2.25z" />
+</svg>
+                <span>Swimming Pool</span>
+                </label>
+          
+                <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+                <input type="checkbox" checked={selected.includes('restaurant')} name="restaurant" onChange={handleCbClick}/>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+</svg>
+                <span>Restaurant</span>
+                </label>
+            
+                <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+                <input type="checkbox" checked={selected.includes('reading')}  name="reading" onChange={handleCbClick}/>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
+</svg>
+                <span>Reading Room</span>
+                </label>
+        </>
+    );
+
+}
+
+export default Perks;
